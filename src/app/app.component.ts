396740f3fcd1becce78b2c2f9029601c74f15f76
kauatwn <kaualopescontato@gmail.com>
2025-07-02 12:49:22 -0300

    Adiciona componentes de relógio e tema ao AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClockComponent } from './features/clock/clock.component';
+import { ThemeToggleComponent } from './shared/components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClockComponent, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'angular-digital-clock-demo';
